64f3349fe4a5d7bc4d6a106df64322ecca5a021f
Fedot Kriutchenko <fodyadev@gmail.com>
2026-04-19 07:02:43 +0000

    ..

--- tools/push.js
+++ tools/push.js
@@ -5,6 +5,7 @@ const root = path.resolve(path.dirname(new URL(import.meta.url).pathname), '..')
 cd(root);
 
 await $`deno run -A tools/check.js`;
+await $`deno run -A tools/bump-version.js`;
 
 const scriptsBin = path.resolve(root, '..', 'webdetta-scripts', 'bin', 'push-package.sh');
 await $`bash ${scriptsBin} ${root} ${process.argv.slice(2)}`;
